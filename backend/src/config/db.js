779446fed9aa71d626af7c676c983a232d0a1c34
Felix Gustavo <gustavovieira1945@gmail.com>
2020-05-25 12:14:47 -0300

    updated dotenv settings and added example

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOSTNAME,
-  dialect: 'mysql',
-  define: { timestamps: false }
+  define: {
+    timestamps: false
+  }
 }
